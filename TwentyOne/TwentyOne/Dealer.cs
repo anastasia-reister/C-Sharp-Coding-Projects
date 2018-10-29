53e2d226649730edbeaf7c35dee0c73d0c1f7d0d
Anastasia Reister <anastasia.reister@gmail.com>
2018-10-29 13:01:05 -0700

    add timestamp to log

--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -20,6 +20,8 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\Nastya\Documents\GitHub\C-Sharp-Coding-Projects\log.txt", true))
             {
+                file.Write(DateTime.Now + " ");
+              
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
